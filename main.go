f60ae29c1e35ea9ecf87fe99723ad07a779657f8
Siddharth Priyadarshi <sidmail4606@gmail.com>
2023-05-06 21:05:04 +0530

    create model for course

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+//Model for course
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Filename string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fakeDB
+var course []Course
+
+// middleware, helper - file
+func IsEmpty(c *Course) bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
+
 func main() {
 	fmt.Println("We are building API in golang")
 
